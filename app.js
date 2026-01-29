44808a956e068c3080b3d8e470c424c4ce579ca4
ASTRO01-max <mfulsefer@gmail.com>
2026-01-29 10:51:30 +0500

    to do list

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+let form = document.querySelector(".form")
 let list = document.querySelector(".list")
 let input = document.querySelector(".input")
 let btn = document.querySelector(".btn")
+let yaratildi = document.querySelector(".yaratildi")
 
+btn.setAttribute('disabled', 'true')
+
+function yaratildiFunc() {
+    yaratildi.textContent = list.children.length
+}
 
 input.addEventListener("input", () => {
     if (input.value.trim() === "") {
@@ -13,9 +20,35 @@ input.addEventListener("input", () => {
 })
 
 
-btn.addEventListener('click', () => {
-    let item = document.createElement("li")
-    item.textContent = input.value
-    list.appendChild(item)
+form.addEventListener('submit', (e) => {
+    // yangilanishni oldini oladi
+    e.preventDefault()
+    const value = input.value.trim()
+    if (value === "") {
+        btn.setAttribute('disabled', 'true')
+        return
+    }
+    const li = document.createElement('li')
+    const icon = document.createElement('i')
+    const checkbox = document.createElement("input")
+    const text = document.createTextNode(value)
+    const p = document.createElement('p')
+    checkbox.type = 'checkbox'
+    icon.classList = 'fa-solid fa-trash icon'
+    p.classList = "text"
+    icon.addEventListener('click', () => {
+        li.remove()
+        yaratildiFunc()
+    })
+    checkbox.addEventListener("change", () => {
+        p.classList.toggle('completed')
+    })
+    p.appendChild(text)
+    li.appendChild(checkbox)
+    li.appendChild(p)
+    li.appendChild(icon)
+    list.appendChild(li)
     input.value = ""
+    yaratildiFunc()
+    btn.disabled = true
 })
